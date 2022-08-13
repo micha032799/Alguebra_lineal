a517f3c45db0c0e86b41350a1f84dbdf7102a42b
Kendall-22 <ken.araya.solis@gmail.com>
2022-08-12 20:18:51 -0600

    Commite

--- algebra/src/main/java/com/mycompany/algebra_lineal/NewMain.java
+++ algebra/src/main/java/com/mycompany/algebra_lineal/NewMain.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package com.mycompany.algebra_lineal;
 
-/**
- *
- * @author micha
- */
+
 public class NewMain {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-//      Inicio
+        System.out.print("Michael zorra");
+
     }
     
 }
